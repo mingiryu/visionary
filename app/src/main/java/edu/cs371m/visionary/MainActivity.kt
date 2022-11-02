82ecedd0391ce4f300519c9b208fb2370fe86db5
mingiryu <mingi@opensyllabus.org>
2022-11-02 06:33:31 -0400

    Setup binding

--- app/src/main/java/edu/cs371m/visionary/MainActivity.kt
+++ app/src/main/java/edu/cs371m/visionary/MainActivity.kt
@@ -2,10 +2,23 @@ package edu.cs371m.visionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import edu.cs371m.visionary.databinding.ActivityMainBinding
+import edu.cs371m.visionary.databinding.ContentMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ContentMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.toolbar)
+        binding = activityMainBinding.contentMain
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(window.decorView.rootView.windowToken, 0)
     }
 }
